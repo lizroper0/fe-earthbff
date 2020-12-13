7392060776c36e8a164fa0591ed7a691dd2e44bf
lizbarnett0 <lizcroper@gmail.com>
2020-12-13 14:58:37 -0600

    scaffolded front end for user login/signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+
 import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
 import Calculator from './Components/Calculator/Calculator';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
+import Resources from './Components/Resources/Resources';
 
 function App() {
 	const [calculator, setCalculator] = useState([]);
@@ -19,6 +24,9 @@ function App() {
 						<Calculator calculator={calculator} setCalculator={setCalculator} />
 					)}
 				/>
+				<Route exact path='/login' render={() => <Login />} />
+				<Route exact path='/signup' render={() => <Signup />} />
+				{/* <Route exact path='/resources' render={() => <Resources />} /> */}
 			</main>
 			<footer>
 				<p>&copy; 2020 Lucky Lizard Technologies</p>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './login.scss';
+
+import { Link } from 'react-router-dom';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	function validateForm() {
+		return email.length > 0 && password.length > 0;
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	return (
+		<div className='login-page'>
+			<div className='login-container'>
+				<div className='form-container'>
+					<Form onSubmit={handleSubmit}>
+						<Form.Group size='lg' controlId='email'>
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								autoFocus
+								type='email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</Form.Group>
+						<Form.Group size='lg' controlId='password'>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</Form.Group>
+						<Button type='submit' disabled={!validateForm()}>
+							Login
+						</Button>
+					</Form>
+					<Link to={'/signup'}>
+						<p> New User? Sign Up Here</p>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
